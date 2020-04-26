33810d43e939df98660b3eee82ceca8f1e34ef4b
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-26 16:40:37 -0500

    Removed js redundancies.

--- javascript/taryn-mckenzie.js
+++ javascript/taryn-mckenzie.js
@@ -1,11 +1,15 @@
 "use strict";
 
 
-function projectCards(project) { //put the coffees into a table
+function projectCards(project) { //put the projects on the page!
 
     let html = '<div class="card-body" id="project-body">'; // create the card body tags
     html += '<div class="card-img">' + project.img + '</div>'; // get the project image
-    html += '<div class="card-title" >' + project.title + '</div>'; // get project title
+    html += '<div class="modal-hidden">' +
+                '<div class="card-modal-img">' + project.img + '</div>' +
+                '<div class="card-modal-title" >' + project.title + '</div>' +
+                '<div class="card-modal-desc" >' + project.desc + '</div>' +
+            '</div>'; // Project Modal to show on click
     html += '</div>'; // end table row
 
     return html; //return the div
@@ -25,7 +29,10 @@ const projects = [ // An array of projects
         id: 1,
         title: 'Coffee Project',
         img: '<img src="img/ben-kolde-Ajcipx1VDXI-unsplash.jpg" class="image"  alt="">',
-        desc: 'This is the project description.'
+        desc: 'This is the project description.',
+        categories: [
+
+        ]
     },
     {
         id: 2,
@@ -66,52 +73,41 @@ const projects = [ // An array of projects
 ];
 
 
-function codepenCards(project) { //put the coffees into a table
-
-    let html = '<div class="card-body" id="project-body">'; // create the card body tags
-    html += '<a target="_blank" href=' + project.url + '>';
-    html += '<div class="card-img">' + project.img + '</div></a>'; // get the project image
-    html += '<div class="card-title" >' + project.title + '</div>'; // get project title
-    html += '</div>'; // end table row
-
-    return html; //return the div
-}
-
-function renderCodepenCards(projects) { //loops through projects, add each project to html
-    let html = '';
-    for(let i = 0; i <= projects.length - 1; i++) {
-        html += codepenCards(projects[i]);
-    }
-    return html;
-}
-
-
 const codepenProjects = [ // An array of codepen projects
     {
         id: 1,
         title: 'Login Form',
         img: '<img src="img/codepen-form-login.png" class="image"  alt="Login form snapshot from Codepen">',
-        url:'"https://codepen.io/tarynmckenzie/full/vYNxzjQ"'
+        url:'"https://codepen.io/tarynmckenzie/full/vYNxzjQ"',
+        desc: 'This is the project description.'
     },
     {
         id: 2,
         title: 'Search Bar',
-        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">'
+        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">',
+        desc: 'This is the project description.'
     },
     {
         id: 3,
         title: 'Calculator',
-        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">'
+        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">',
+        desc: 'This is the project description.'
     },
     {
         id: 4,
         title: 'Card',
-        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">'
+        img: '<img src="https://placeholder.pics/svg/250x200" class="image" alt="">',
+        desc: 'This is the project description.'
     },
 ];
+
 
 const portfolioProjectsSection = document.querySelector('#project-cards'); // get the id for the card div
 portfolioProjectsSection.innerHTML = renderProjectCards(projects); // place cards into queried div
 
 const codepenProjectsSection = document.querySelector('#codepen-cards'); // get the id for the card div
-codepenProjectsSection.innerHTML = renderCodepenCards(codepenProjects); // place cards into queried div
+codepenProjectsSection.innerHTML = renderProjectCards(codepenProjects); // place cards into queried div
+
+document.getElementById('card-img').onclick=function(){
+    document.getElementById('modal-hidden').style.display='block';
+};
