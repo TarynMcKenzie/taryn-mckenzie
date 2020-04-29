f7f515b39cca1933312ac28398fa826e67760876
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-28 21:29:12 -0500

    Gallery functioning. Modal displaying same card for all, need fix

--- portfolio-gallery.js
+++ portfolio-gallery.js
@@ -4,32 +4,36 @@
 // LOAD DOM
 $(document).ready(function () { //Run jQuery after document loads
 
+//-----------------------------------------
+//--------SHOW PROJECTS FUNCTION-----------
+//-----------------------------------------
     // TODO: Create a function that structures the project cards onto the page
     function projectCards(projects) {
-        console.log(projects)
+
         $.each(projects, function (index, currentProject) { // Select the projects input --> Iterate through each project
 
-            console.log(index + currentProject.img); // test
-
             let html = '<div class="card-body" id="project-body">'; // create the card body
-
             html += '<div class="card-img">' + currentProject.img + '</div>'; // get the project image
+
             html += '<div class="modal">' +
-                    '<div class="modal-content">' +
-                    '<div class="card-modal-img">' + currentProject.img + '</div>' +
-                    '<div class="close-btn" ><span class="close-btn">&times;</span></div>' +
-                    '<div class="card-modal-title" >' + currentProject.title + '</div>' +
-                    '<div class="card-modal-desc" >' + currentProject.desc + '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>';
+                '<div class="modal-content">' +
+                '<div class="card-modal-img">' + currentProject.img + '</div>' +
+                '<div class="close-btn" ><span class="close-btn">&times;</span></div>' +
+                '<div class="card-modal-title" >' + currentProject.title + '</div>' +
+                '<div class="card-modal-desc" >' + currentProject.desc + '</div>' +
+                '</div>' +
+                '</div>' +
+
+                '</div>';
 
             $('#project-cards').append(html);
         })
 
     }
 
-
+//-----------------------------------------
+//-----------PROJECTS TO SHOW--------------
+//-----------------------------------------
     // TODO: Create an array of project objects
     const projects = [ // An array of projects
         {
@@ -76,38 +80,8 @@ $(document).ready(function () { //Run jQuery after document loads
         },
     ];
 
-    // TODO: Create an array of codepen project objects
-    const codepenProjects = [ // An array of codepen projects
-        {
-            id: 1,
-            title: 'Login Form',
-            img: '<img src="img/codepen-form-login.png" class="image"  alt="Login form snapshot from Codepen">',
-            url: '"https://codepen.io/tarynmckenzie/full/vYNxzjQ"',
-            desc: 'This is the project description.'
-        },
-        {
-            id: 2,
-            title: 'Search Bar',
-            img: '<img src="img/tyler-lastovich-e31ANd1PXUw-unsplash.jpg" class="image" alt="">',
-            desc: 'This is the project description.'
-        },
-        {
-            id: 3,
-            title: 'Calculator',
-            img: '<img src="img/ben-kolde-Ajcipx1VDXI-unsplash.jpg" class="image" alt="">',
-            desc: 'This is the project description.'
-        },
-        {
-            id: 4,
-            title: 'Card',
-            img: '<img src="img/tyler-lastovich-e31ANd1PXUw-unsplash.jpg" class="image" alt="">',
-            desc: 'This is the project description.'
-        },
-    ];
-
     // TODO: Call the function to put the project objects onto the page
     projectCards(projects);
-    // projectCards(codepenProjects);
 
 //-----------------------------------------
 //-----------MODAL FUNCTIONS---------------
@@ -121,15 +95,9 @@ $(document).ready(function () { //Run jQuery after document loads
     });
 
 // Hide modal on click
-    $('.close-btn').click(function () { // when the close icon 'x' is clicked
+    $('.close-btn').click(function () { // when an image is clicked
 
-        $('.close-btn').css('display', 'none'); // HIDE the modal view
-
-    });
-
-    $('body').click(function (event) { // when the window is clicked
-
-        $('modal').css('display', 'none'); // HIDE the modal view
+        $('.modal').css('display', 'none'); // SHOW the modal view
 
     });
 
