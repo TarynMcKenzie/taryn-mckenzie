8bf1f369944e82a4b2be041ca6c23bdecd4e7582
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2021-02-06 15:41:53 -0600

    removed files, added placeholder elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+// import React, { useState, useEffect } from "react";
+import "./App.css";
+
+// === COMPONENTS ===
 
 function App() {
+  // === USE STATES ===
+  
+  // ===  ===
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Paragraph text goes here.</p>
-      </header>
+      <nav>
+        <ul>
+          <li><a href="#">Taryn McKenzie.</a></li>
+        </ul>
+      </nav>
+      <article>
+        <section>
+          <header>
+            <h1>Websites designed with intention.</h1>
+            <p>
+              Device's command support detection is in progress. The maximum
+              number of sessions has been reached. An operation is not supported
+              on a directory. This version of %1 is not compatible with the
+              version of Windows you're running. Check your computer's system
+              information and then contact the software publisher. The network
+              is busy. The specified network password is not correct. An
+              operation is currently in progress with the device. The name limit
+              for the local computer network adapter card was exceeded.
+            </p>
+            <button>Learn More</button>
+          </header>
+        </section>
+        <section></section>
+      </article>
     </div>
   );
 }
